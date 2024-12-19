a9e42850aa2f56a17a1deaeb41b22575b5d12b93
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-19 08:10:05 -0800

    update code

--- lib/views/Create-Profile-Views/alergic0medications/alergic-medication.dart
+++ lib/views/Create-Profile-Views/alergic0medications/alergic-medication.dart
@@ -1,4 +1,3 @@
-
 //import 'package:flutter/cupertino.dart';
 //import 'package:flutter/material.dart';
 
@@ -16,10 +15,12 @@
 // }
 
 import 'package:ecare/constants/linker.dart';
+import 'package:ecare/views/Create-Profile-Views/alergic0medications/alergic-medications.dart';
 
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 
-class AlergicMedications extends StatefulWidget {
+class AlergicMedications extends StatelessWidget {
   final String? role;
   final String? userId;
   final Map<String, dynamic>? userProfileData;
@@ -32,9 +33,117 @@ class AlergicMedications extends StatefulWidget {
     this.userId,
   });
 
+  final AllergicMedicationsController controller =
+      Get.put(AllergicMedicationsController());
+
   @override
-  State<AlergicMedications> createState() => _AlergicMedicationsState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: BoldTextWidgetTheme(text: 'Create Profile'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              controller.storeUserData(userId);
+              print('object=${controller.allergicMedicationsData}');
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => CurrentOngoingMedications(
+                    role: role!,
+                    userID: userId!,
+                    userProfileData: userProfileData!,
+                    comorbiditiesData: comorbiditiesData!,
+                    allergicMedicationsData: controller.allergicMedicationsData,
+                  ),
+                ),
+              );
+            },
+            child: Text(
+              'Skip',
+              style: TextStyle(color: Colors.blueAccent.shade700),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+          child: Column(
+            children: [
+              BoldTextWidgetTheme(text: 'Allergic Medications'),
+              LightDarktextField(
+                controller: controller.medicineController1,
+                hintText: 'Name of the medicine',
+                keyboardType: TextInputType.text,
+              ),
+              LightDarktextField(
+                controller: controller.medicineController2,
+                hintText: 'Name of the medicine',
+                keyboardType: TextInputType.text,
+              ),
+              LightDarktextField(
+                controller: controller.medicineController3,
+                hintText: 'Name of the medicine',
+                keyboardType: TextInputType.text,
+              ),
+              LightDarktextField(
+                controller: controller.medicineController4,
+                hintText: 'Name of the medicine',
+                keyboardType: TextInputType.text,
+              ),
+              LightDarktextField(
+                controller: controller.medicineController5,
+                hintText: 'Name of the medicine',
+                keyboardType: TextInputType.text,
+              ),
+              InkWell(
+                onTap: () {
+                  controller.storeUserData(userId);
+                  print('object=${controller.allergicMedicationsData}');
+                  Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                      builder: (context) => CurrentOngoingMedications(
+                        role: role!,
+                        userID: userId!,
+                        userProfileData: userProfileData!,
+                        comorbiditiesData: comorbiditiesData!,
+                        allergicMedicationsData:
+                            controller.allergicMedicationsData,
+                      ),
+                    ),
+                  );
+                },
+                child: BlueButton(
+                  text: 'Next',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
+
+// class AlergicMedications extends StatefulWidget {
+//   final String? role;
+//   final String? userId;
+//   final Map<String, dynamic>? userProfileData;
+//   final Map<String, dynamic>? comorbiditiesData;
+
+//   AlergicMedications({
+//     this.userProfileData,
+//     this.role,
+//     this.comorbiditiesData,
+//     this.userId,
+//   });
+
+//   @override
+//   State<AlergicMedications> createState() => _AlergicMedicationsState();
+// }
 
 // class _AlergicMedicationsState extends State<AlergicMedications> {
 //   Map<String, dynamic> allergicMedicationsData = {};
@@ -201,138 +310,139 @@ class AlergicMedications extends StatefulWidget {
 //   }
 // }
 
-class _AlergicMedicationsState extends State<AlergicMedications> {
-  Map<String, dynamic> allergicMedicationsData = {};
+///---fully worked code for web
+// class _AlergicMedicationsState extends State<AlergicMedications> {
+//   Map<String, dynamic> allergicMedicationsData = {};
 
-  final TextEditingController medicineController1 = TextEditingController();
-  final TextEditingController medicineController2 = TextEditingController();
-  final TextEditingController medicineController3 = TextEditingController();
-  final TextEditingController medicineController4 = TextEditingController();
-  final TextEditingController medicineController5 = TextEditingController();
+//   final TextEditingController medicineController1 = TextEditingController();
+//   final TextEditingController medicineController2 = TextEditingController();
+//   final TextEditingController medicineController3 = TextEditingController();
+//   final TextEditingController medicineController4 = TextEditingController();
+//   final TextEditingController medicineController5 = TextEditingController();
 
-  void _storeUserData() {
-    allergicMedicationsData['medicineController1'] =
-        medicineController1.text ?? '';
-    allergicMedicationsData['medicineController2'] =
-        medicineController2.text ?? '';
-    allergicMedicationsData['medicineController3'] =
-        medicineController3.text ?? '';
-    allergicMedicationsData['medicineController4'] =
-        medicineController4.text ?? '';
-    allergicMedicationsData['medicineController5'] =
-        medicineController5.text ?? '';
-    allergicMedicationsData['userId'] = widget.userId;
-  }
+//   void _storeUserData() {
+//     allergicMedicationsData['medicineController1'] =
+//         medicineController1.text ?? '';
+//     allergicMedicationsData['medicineController2'] =
+//         medicineController2.text ?? '';
+//     allergicMedicationsData['medicineController3'] =
+//         medicineController3.text ?? '';
+//     allergicMedicationsData['medicineController4'] =
+//         medicineController4.text ?? '';
+//     allergicMedicationsData['medicineController5'] =
+//         medicineController5.text ?? '';
+//     allergicMedicationsData['userId'] = widget.userId;
+//   }
 
-  @override
-  void dispose() {
-    medicineController1.dispose();
-    medicineController2.dispose();
-    medicineController3.dispose();
-    medicineController4.dispose();
-    medicineController5.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     medicineController1.dispose();
+//     medicineController2.dispose();
+//     medicineController3.dispose();
+//     medicineController4.dispose();
+//     medicineController5.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: BoldTextWidgetTheme(text: 'Create Profile'),
-        actions: [
-          //widget.role=='register'?
-          TextButton(
-            onPressed: () {
-              _storeUserData();
-              print('object=$allergicMedicationsData');
-              Navigator.push(
-                context,
-                CupertinoPageRoute(
-                  builder: (context) => CurrentOngoingMedications(
-                    role: widget.role!,
-                    userID: widget.userId!,
-                    userProfileData: widget.userProfileData!,
-                    comorbiditiesData: widget.comorbiditiesData!,
-                    allergicMedicationsData: allergicMedicationsData!,
-                  ),
-                ),
-              );
-            },
-            child: Text(
-              'Skip',
-              style: TextStyle(color: Colors.blueAccent.shade700),
-            ),
-          )
-          //  :SizedBox()
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
-          child: Column(
-            children: [
-              BoldTextWidgetTheme(text: 'Allergic Medications'),
-              LightDarktextField(
-                controller: medicineController1,
-                hintText: 'Name of the medicine',
-                keyboardType: TextInputType.text,
-              ),
-              LightDarktextField(
-                controller: medicineController2,
-                hintText: 'Name of the medicine',
-                keyboardType: TextInputType.text,
-              ),
-              LightDarktextField(
-                controller: medicineController3,
-                hintText: 'Name of the medicine',
-                keyboardType: TextInputType.text,
-              ),
-              LightDarktextField(
-                controller: medicineController4,
-                hintText: 'Name of the medicine',
-                keyboardType: TextInputType.text,
-              ),
-              LightDarktextField(
-                controller: medicineController5,
-                hintText: 'Name of the medicine',
-                keyboardType: TextInputType.text,
-              ),
-              InkWell(
-                onTap: () {
-                  allergicMedicationsData['medicine1'] =
-                      medicineController1.text;
-                  allergicMedicationsData['medicine2'] =
-                      medicineController2.text;
-                  allergicMedicationsData['medicine3'] =
-                      medicineController3.text;
-                  allergicMedicationsData['medicine4'] =
-                      medicineController4.text;
-                  allergicMedicationsData['medicine5'] =
-                      medicineController5.text;
-                  _storeUserData();
-                  print('object=$allergicMedicationsData');
-                  Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                      builder: (context) => CurrentOngoingMedications(
-                        role: widget.role!,
-                        userID: widget.userId!,
-                        userProfileData: widget.userProfileData!,
-                        comorbiditiesData: widget.comorbiditiesData!,
-                        allergicMedicationsData: allergicMedicationsData,
-                      ),
-                    ),
-                  );
-                },
-                child: BlueButton(
-                  text: 'Next',
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.transparent,
+//         title: BoldTextWidgetTheme(text: 'Create Profile'),
+//         actions: [
+//           //widget.role=='register'?
+//           TextButton(
+//             onPressed: () {
+//               _storeUserData();
+//               print('object=$allergicMedicationsData');
+//               Navigator.push(
+//                 context,
+//                 CupertinoPageRoute(
+//                   builder: (context) => CurrentOngoingMedications(
+//                     role: widget.role!,
+//                     userID: widget.userId!,
+//                     userProfileData: widget.userProfileData!,
+//                     comorbiditiesData: widget.comorbiditiesData!,
+//                     allergicMedicationsData: allergicMedicationsData!,
+//                   ),
+//                 ),
+//               );
+//             },
+//             child: Text(
+//               'Skip',
+//               style: TextStyle(color: Colors.blueAccent.shade700),
+//             ),
+//           )
+//           //  :SizedBox()
+//         ],
+//       ),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+//           child: Column(
+//             children: [
+//               BoldTextWidgetTheme(text: 'Allergic Medications'),
+//               LightDarktextField(
+//                 controller: medicineController1,
+//                 hintText: 'Name of the medicine',
+//                 keyboardType: TextInputType.text,
+//               ),
+//               LightDarktextField(
+//                 controller: medicineController2,
+//                 hintText: 'Name of the medicine',
+//                 keyboardType: TextInputType.text,
+//               ),
+//               LightDarktextField(
+//                 controller: medicineController3,
+//                 hintText: 'Name of the medicine',
+//                 keyboardType: TextInputType.text,
+//               ),
+//               LightDarktextField(
+//                 controller: medicineController4,
+//                 hintText: 'Name of the medicine',
+//                 keyboardType: TextInputType.text,
+//               ),
+//               LightDarktextField(
+//                 controller: medicineController5,
+//                 hintText: 'Name of the medicine',
+//                 keyboardType: TextInputType.text,
+//               ),
+//               InkWell(
+//                 onTap: () {
+//                   allergicMedicationsData['medicine1'] =
+//                       medicineController1.text;
+//                   allergicMedicationsData['medicine2'] =
+//                       medicineController2.text;
+//                   allergicMedicationsData['medicine3'] =
+//                       medicineController3.text;
+//                   allergicMedicationsData['medicine4'] =
+//                       medicineController4.text;
+//                   allergicMedicationsData['medicine5'] =
+//                       medicineController5.text;
+//                   _storeUserData();
+//                   print('object=$allergicMedicationsData');
+//                   Navigator.push(
+//                     context,
+//                     CupertinoPageRoute(
+//                       builder: (context) => CurrentOngoingMedications(
+//                         role: widget.role!,
+//                         userID: widget.userId!,
+//                         userProfileData: widget.userProfileData!,
+//                         comorbiditiesData: widget.comorbiditiesData!,
+//                         allergicMedicationsData: allergicMedicationsData,
+//                       ),
+//                     ),
+//                   );
+//                 },
+//                 child: BlueButton(
+//                   text: 'Next',
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/views/Create-Profile-Views/alergic0medications/alergic-medications.dart
+++ lib/views/Create-Profile-Views/alergic0medications/alergic-medications.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 
+class AllergicMedicationsController extends GetxController {
+  // Text controllers
+  final medicineController1 = TextEditingController();
+  final medicineController2 = TextEditingController();
+  final medicineController3 = TextEditingController();
+  final medicineController4 = TextEditingController();
+  final medicineController5 = TextEditingController();
+
+  // Allergic medications data
+  final RxMap<String, dynamic> allergicMedicationsData =
+      <String, dynamic>{}.obs;
+
+  void storeUserData(String? userId) {
+    allergicMedicationsData['medicineController1'] = medicineController1.text;
+    allergicMedicationsData['medicineController2'] = medicineController2.text;
+    allergicMedicationsData['medicineController3'] = medicineController3.text;
+    allergicMedicationsData['medicineController4'] = medicineController4.text;
+    allergicMedicationsData['medicineController5'] = medicineController5.text;
+    allergicMedicationsData['userId'] = userId;
+  }
+
+  @override
+  void onClose() {
+    // Dispose controllers
+    medicineController1.dispose();
+    medicineController2.dispose();
+    medicineController3.dispose();
+    medicineController4.dispose();
+    medicineController5.dispose();
+    super.onClose();
+  }
+}

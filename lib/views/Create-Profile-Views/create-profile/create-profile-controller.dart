6083504d639bf4ff592b53c69be3bceadddb8f14
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-19 07:45:41 -0800

    up

--- lib/views/Create-Profile-Views/create-profile/create-profile-controller.dart
+++ lib/views/Create-Profile-Views/create-profile/create-profile-controller.dart
@@ -22,28 +22,35 @@ import 'package:intl/intl.dart';
 import 'package:ecare/constants/linker.dart';
 import 'package:image_picker/image_picker.dart';
 
-class CreateProfileController extends GetxController {
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+class ProfileController extends GetxController {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final RxMap<String, dynamic> userProfileData = <String, dynamic>{}.obs;
 
-  // TextEditingController instances
-  final nameController = TextEditingController();
-  final primaryContactController = TextEditingController();
-  final emergencyContact1Controller = TextEditingController();
-  final emergencyContact2Controller = TextEditingController();
-  final dateController = TextEditingController();
-
-  // Reactive variables
-  var selectedGender = ''.obs;
-  var emergencyContact1Relation = ''.obs;
-  var emergencyContact2Relation = ''.obs;
-  var selectedBloodGroup = ''.obs;
-
-  var userID = ''.obs;
-  var userEmail = ''.obs;
-  var imagePath = ''.obs; // To store Base64 image data
-  final Rx<XFile?> imageFile = Rx<XFile?>(null);
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController primaryContactController =
+      TextEditingController();
+  final TextEditingController emergencyContact1Controller =
+      TextEditingController();
+  final TextEditingController emergencyContact2Controller =
+      TextEditingController();
+  final TextEditingController dateController = TextEditingController();
 
   final ImagePicker picker = ImagePicker();
+  final Rx<XFile?> selectedImage = Rx<XFile?>(null);
+  final RxString imagePath = ''.obs;
+
+  String userID = '';
+  String userEmail = '';
 
   @override
   void onInit() {
@@ -54,8 +61,8 @@ class CreateProfileController extends GetxController {
   Future<void> fetchUser() async {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      userID.value = user.uid;
-      userEmail.value = user.email ?? '';
+      userID = user.uid;
+      userEmail = user.email!;
     }
   }
 
@@ -64,61 +71,53 @@ class CreateProfileController extends GetxController {
       final pickedFile = await picker.pickImage(source: ImageSource.gallery);
       if (pickedFile != null) {
         final bytes = await pickedFile.readAsBytes();
-        imageFile.value = pickedFile;
-        imagePath.value = base64Encode(bytes); // Save as Base64 string
-        Get.snackbar("Success", "Image selected successfully!");
-      } else {
-        Get.snackbar("No Image", "No image selected.");
+        selectedImage.value = pickedFile;
+        imagePath.value = base64Encode(bytes);
       }
     } catch (e) {
-      Get.snackbar("Error", "Failed to pick image: $e");
+      print('Error picking image: $e');
     }
   }
 
   void storeUserData() {
     if (formKey.currentState?.validate() ?? false) {
-      Map<String, dynamic> userProfileData = {
-        'name': nameController.text,
-        'primaryContact': primaryContactController.text,
-        'emergencyContact1': emergencyContact1Controller.text,
-        'relation1': emergencyContact1Relation.value,
-        'emergencyContact2': emergencyContact2Controller.text,
-        'relation2': emergencyContact2Relation.value,
-        'gender': selectedGender.value,
-        'bloodGroup': selectedBloodGroup.value,
-        'dateOfBirth': dateController.text,
-        'profileImage': imagePath.value,
-        'userId': userID.value,
-        'email': userEmail.value,
-        'refferId': _generateRandomCode(),
-      };
+      // Populate userProfileData map
+      userProfileData['name'] = nameController.text;
+      userProfileData['primaryContact'] = primaryContactController.text;
+      userProfileData['emergencyContact1'] = emergencyContact1Controller.text;
+      userProfileData['emergencyContact2'] = emergencyContact2Controller.text;
+      userProfileData['dateOfBirth'] = dateController.text;
+      userProfileData['profileImage'] = imagePath.value;
+      userProfileData['userId'] = userID;
+      userProfileData['email'] = userEmail;
+      userProfileData['refferId'] = generateRandomCode();
 
       FirebaseFirestore.instance
           .collection('Users')
-          .doc(userID.value)
-          .set(userProfileData)
+          .doc(userID)
+          .set(userProfileData.value) // Access the Map with `.value`
           .then((_) {
-        Get.snackbar("Success", "User profile stored successfully!");
+        print('User data stored successfully.');
         Get.to(() => ComorbitiesView(
-              role: "user",
-              userProfileData: userProfileData,
-              userID: userID.value,
+              role: 'user', // Replace with actual role if needed
+              userProfileData: userProfileData.value,
+              userID: userID,
             ));
       }).catchError((error) {
-        Get.snackbar("Error", "Failed to store user profile: $error");
+        print('Error storing user data: $error');
       });
     }
   }
 
-  String _generateRandomCode() {
+  String generateRandomCode() {
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numbers = '0123456789';
-    var random = Random();
+    final random = Random();
 
     String randomDigits =
-        List.generate(4, (index) => numbers[random.nextInt(10)]).join();
+        List.generate(4, (_) => numbers[random.nextInt(10)]).join();
     String randomAlphabets =
-        List.generate(3, (index) => letters[random.nextInt(26)]).join();
+        List.generate(3, (_) => letters[random.nextInt(26)]).join();
 
     return randomDigits + randomAlphabets;
   }
